a94b5630397f59768214a30d4ba45f3e0997e4da
Yair Ziv <zivyair06@gmail.com>
2018-01-04 22:42:38 +0200

    Finished the first 4 rounds of the memory module.

--- Modules/Memory.py
+++ Modules/Memory.py
@@ -51,6 +51,9 @@ def round_one():
         return
 
 def round_two():
+    """
+    Round 2 solution
+    """
     sequence = raw_input('Enter the current sequence\n')
     indicator = int(sequence[0])
 
@@ -58,27 +61,96 @@ def round_two():
         for index, value in enumerate(sequence):
             if value == '4':
                 print 'Press the button in position {}'.format(index)
+                position_list.append(index)
+                label_list.append('4')
                 return
 
     if indicator == 2:
         print 'Press the button in position {}'.format(position_list[0])
+        position_list.append(position_list[0])
+        label_list.append(sequence[position_list[0]])
         return
 
     if indicator == 3:
         print 'Press the button in position 1'
+        position_list.append(1)
+        label_list.append(sequence[1])
         return
 
     if indicator == 4:
         print 'Press the button in position {}'.format(position_list[0])
-        position_list.append(4)
-        label_list.append(sequence[4])
+        position_list.append(position_list[0])
+        label_list.append(sequence[position_list[0]])
         return
 
 def round_three():
-    pass
+    """
+    Round 3 solution
+    """
+    sequence = raw_input('Enter the current sequence:\n')
+    indicator = int(sequence[0])
+
+    if indicator == 1:
+        for index, value in enumerate(sequence[1:]):
+            if value == label_list[1]:
+                print 'Press the button in position {}'.format(index + 1)
+                position_list.append(index + 1)
+                label_list.append(label_list[1])
+                return
+
+    if indicator == 2:
+        for index, value in enumerate(sequence[1:]):
+            if value == label_list[0]:
+                print 'Press the button in position {}'.format(index + 1)
+                position_list.append(index + 1)
+                label_list.append(label_list[0])
+                return
+
+    if indicator == 3:
+        print 'Press the button in position 3'
+        position_list.append(3)
+        label_list.append(sequence[3])
+        return
+
+    if indicator == 3:
+        for index, value in enumerate(sequence[1:]):
+            if value == '4':
+                print 'Press the button in position {}'.format(index + 1)
+                position_list.append(index + 1)
+                label_list.append('4')
+                return
+
 
 def round_four():
-    pass
+    """
+    Round 4 solution
+    """
+    sequence = raw_input('Enter the current sequence:\n')
+    indicator = int(sequence[0])
+    
+    if indicator == 1:
+        print 'Press the button in position {}'.format(position_list[0])
+        position_list.append(position_list[0])
+        label_list.append(sequence[position_list[0]])
+        return
+
+    if indicator == 2:
+        print 'Press the button in position 1'
+        position_list.append(1)
+        label_list.append(sequence[1])
+        return
+
+    if indicator == 3:
+        print 'Press the button in position {}'.format(position_list[1])
+        position_list.append(position_list[1])
+        label_list.append(sequence[position_list[1]])
+        return
+
+    if indicator == 4:
+        print 'Press the button in position {}'.format(position_list[1])
+        position_list.append(position_list[1])
+        label_list.append(sequence[position_list[1]])
+        return
 
 def round_five():
     pass
